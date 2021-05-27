019173151a5746c118374ec2d05913541e82f1ec
Rafael Sene <rpsene@br.ibm.com>
2021-05-27 15:33:43 -0300

    Update multiarch.sh

--- tools/multiarch.sh
+++ tools/multiarch.sh
@@ -11,4 +11,4 @@ docker manifest create quay.io/powercloud/powervs-actions:rh-ocp-database \
 quay.io/powercloud/powervs-actions:rh-ocp-database-x86_64 quay.io/powercloud/powervs-actions:rh-ocp-database-ppc64le
 
 docker login quay.io -u "$USER_QUAY" -p "$PWD_QUAY"
-docker manifest push quay.io/powercloud/powervs-actions:delete-ocp-cluster
+docker manifest push quay.io/powercloud/powervs-actions:rh-ocp-database
